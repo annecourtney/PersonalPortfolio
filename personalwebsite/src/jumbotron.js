7426cfa624d3a416210f901c00809c268a8c1dc6
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-11 18:33:42 -0500

    added github icon

--- personalwebsite/src/jumbotron.js
+++ personalwebsite/src/jumbotron.js
@@ -9,10 +9,14 @@ const JumbotronIntro = () => {
             <p className="text">I’m a full-stack Engineer with a passion for learning, design, and improving the quality of life for users.</p>
 
             <p className="text">Lately, I’m big into React, Node, Express, MongoDB, and Mongoose. Check out my projects or visit my GitHub to see what I’ve been up to!</p>
-            <p>
-            <Button className="button-1" color="primary">Projects</Button>
-            <Button className="button-2" color="primary">GitHub</Button>
-            </p>
+            <div className="button">
+            <Button className="button-1" color="primary">Projects</Button>  
+            </div>
+            <div className="icon">
+            <i className="fab fa-github fa-2x"></i>
+            </div>
+            
+      
         </Jumbotron>
         </div>
     )
