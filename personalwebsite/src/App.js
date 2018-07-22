2db88a826c4b1ecddad87ed69f1618418fe061f2
Anne Courtney <annecourtney_1@yahoo.com>
2018-07-21 19:04:53 -0500

    style tweaks to background images

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -19,22 +19,24 @@ class App extends Component {
          </ul>
      </nav>
 
-     <aside id="sticky-social">
+    <aside id="sticky-social">
     <ul>
         <li><a href="#" class="entypo-github" target="_blank"><span>Github</span></a></li>
         <li><a href="#" class="entypo-linkedin" target="_blank"><span>LinkedIn</span></a></li>
         <li><a href="#" class="entypo-instagrem" target="_blank"><span>Instagram</span></a></li>
         <li><a href="#" class="entypo-pinterest" target="_blank"><span>Pinterest</span></a></li>
-
     </ul>
-</aside>
+    </aside>
+     {/* paint images for background */}
+     <img className="img1" src="https://i.pinimg.com/originals/e9/00/a8/e900a8286eae835f7badafc713e8d126.png" alt="pic1" />
+    <img className="img2" src="https://i.pinimg.com/originals/06/0c/3d/060c3df66511e3a0def66e9afa3014f7.png" alt="pic2" />
+    <img className="img3" src="https://i.pinimg.com/originals/32/f0/9c/32f09c1c70fdd41238c7fc1fe96ba0ea.png" alt="pic3" />
+    <img className="img4" src="https://i.pinimg.com/originals/48/3d/65/483d6523f9f15faee9da0e8838672438.png" alt="pic4" />
+    
     {/* intro jumbotron */}
     <JumbotronIntro />
 
-    {/* paint images for background */}
-    <img className="img1" src="https://i.pinimg.com/originals/e9/00/a8/e900a8286eae835f7badafc713e8d126.png" alt="pic1" />
-    <img className="img2" src="https://i.pinimg.com/originals/06/0c/3d/060c3df66511e3a0def66e9afa3014f7.png" alt="pic2" />
-    <img className="img3" src="https://i.pinimg.com/originals/32/f0/9c/32f09c1c70fdd41238c7fc1fe96ba0ea.png" alt="pic3" />
+   
     </div>
     );
   }
--- personalwebsite/src/jumbotron.js
+++ personalwebsite/src/jumbotron.js
@@ -5,7 +5,8 @@ const JumbotronIntro = () => {
     return (
         <div>
         <Jumbotron className="jumbotron">
-            <h1 className="j-title">Software Engineer</h1>
+            <h1 className="j-title">Full-Stack</h1>
+            <h2 className="j-title2">Web Developer</h2>
             <p className="text">I’m a full-stack Engineer with a passion for learning, design, and improving the quality of life for users.</p>
 
             <p className="text">Lately, I’m big into React, Node, Express, MongoDB, and Mongoose. Check out my projects or visit my GitHub to see what I’ve been up to!</p>
