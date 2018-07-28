32bd8d0ee0830136876915860397a39dc8db82cd
Anne Courtney <annecourtney_1@yahoo.com>
2018-07-28 15:50:22 -0500

    started on projects component

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import JumbotronIntro from './jumbotron.js';
+import Projects from './projects.js';
 import './App.css';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
     {/* Header Image */}
-      <img className="myHeader" src="https://i.pinimg.com/originals/97/1d/2f/971d2f43dd9a161f50b0a0bc3d00e9fb.png" alt="logoimage" />
+      <img className="myHeader" src="https://i.pinimg.com/originals/51/f0/27/51f0271522934428ab7662fa6e957b7b.png" alt="logoimage" />
     {/* top navigation */}
      <nav>
        <ul>
@@ -28,7 +30,7 @@ class App extends Component {
     </ul>
     </aside>
      {/* paint images for background */}
-     <img className="img1" src="https://i.pinimg.com/originals/e9/00/a8/e900a8286eae835f7badafc713e8d126.png" alt="pic1" />
+    <img className="img1" src="https://i.pinimg.com/originals/e9/00/a8/e900a8286eae835f7badafc713e8d126.png" alt="pic1" />
     <img className="img2" src="https://i.pinimg.com/originals/06/0c/3d/060c3df66511e3a0def66e9afa3014f7.png" alt="pic2" />
     <img className="img3" src="https://i.pinimg.com/originals/32/f0/9c/32f09c1c70fdd41238c7fc1fe96ba0ea.png" alt="pic3" />
     <img className="img4" src="https://i.pinimg.com/originals/48/3d/65/483d6523f9f15faee9da0e8838672438.png" alt="pic4" />
@@ -36,7 +38,9 @@ class App extends Component {
     {/* intro jumbotron */}
     <JumbotronIntro />
 
-   
+    {/* Projects */}
+    <Projects />
+
     </div>
     );
   }
---
+++ personalwebsite/src/projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+        <h1>Projects</h1>
+        <a href="https://vibrant-darwin-54252a.netlify.com/" target="_blank"><img className="project1" src="https://i.pinimg.com/originals/55/2b/0b/552b0bdb3b0a2b09bcc5925bce80b968.png" alt="proj1image" /></a>
+        </div>
+    )
+};
+
+export default Projects;
