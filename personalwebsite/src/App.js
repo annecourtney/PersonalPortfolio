112845f95e501b1c8d592de37df75470755d41d8
Anne Courtney <annecourtney_1@yahoo.com>
2018-08-02 23:54:26 -0500

    added some cards to display project - wip still

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -8,13 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-    {/* Header Image */}
-      <img className="myHeader" src="https://i.pinimg.com/originals/51/f0/27/51f0271522934428ab7662fa6e957b7b.png" alt="logoimage" />
+   
     {/* top navigation */}
      <nav>
        <ul>
          <li>Home</li>
-         <li>About</li>
          <li>Skills</li>
          <li>Projects</li>
          <li>Contact</li>
@@ -29,21 +27,23 @@ class App extends Component {
         <li><a href="#" class="entypo-pinterest" target="_blank"><span>Pinterest</span></a></li>
     </ul>
     </aside>
-     {/* paint images for background */}
+     {/* paint images for background
     <img className="img1" src="https://i.pinimg.com/originals/e9/00/a8/e900a8286eae835f7badafc713e8d126.png" alt="pic1" />
     <img className="img2" src="https://i.pinimg.com/originals/06/0c/3d/060c3df66511e3a0def66e9afa3014f7.png" alt="pic2" />
     <img className="img3" src="https://i.pinimg.com/originals/c9/73/aa/c973aa060e3fdc4087c5c4d00e0cf354.png" alt="pic3" />
     <img className="img4" src="https://i.pinimg.com/originals/48/3d/65/483d6523f9f15faee9da0e8838672438.png" alt="pic4" />
-    
+     */}
     {/* intro jumbotron */}
     <JumbotronIntro />
 
     {/* Computer transitional image */}
     <img className="computer-image" src="https://i.pinimg.com/originals/90/7f/e7/907fe75edd9889db2984699b744f9066.jpg" alt="computerimage" />
-
+    
     {/* Projects */}
     <Projects />
 
+    {/* Contact Image */}
+ <img className="myHeader" src="https://i.pinimg.com/originals/51/f0/27/51f0271522934428ab7662fa6e957b7b.png" alt="logoimage" />
     </div>
     );
   }
--- personalwebsite/src/projects.js
+++ personalwebsite/src/projects.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import { Card, Button, CardImg, CardTitle, CardText, CardDeck,
+    CardSubtitle, CardBody } from 'reactstrap';
 
 const Projects = () => {
     return (
         <div>
+        <CardDeck>
+        <Card>
+          <CardImg top width="35%" src="https://i.pinimg.com/originals/55/2b/0b/552b0bdb3b0a2b09bcc5925bce80b968.png" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg top width="35%" src="https://i.pinimg.com/originals/2b/13/43/2b134324d15167c3dfc957b0392543ae.png" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg top width="35%" src="https://i.pinimg.com/originals/c8/0c/4a/c80c4ae1ffddd5571d94f2d537f36c28.png" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </CardDeck>
+        
         <h1 className="projects-title">Projects</h1>
+
+        <p>Built and designed a simple and sleek 'Note Taking' application from the ground up with React.js on the front-end, and Node.js for the back-end.</p>
         <div className="project1">
         <a href="https://vibrant-darwin-54252a.netlify.com/" target="_blank"><img src="https://i.pinimg.com/originals/55/2b/0b/552b0bdb3b0a2b09bcc5925bce80b968.png" alt="proj1image" /></a>
+        </div>
+
+        <p>Created my first PyGame application using Python.</p>
+        <div className="project2">
+        <a target="_blank"><img src="https://i.pinimg.com/originals/2b/13/43/2b134324d15167c3dfc957b0392543ae.png" alt="proj1image" /></a>
         </div>
         </div>
     )
